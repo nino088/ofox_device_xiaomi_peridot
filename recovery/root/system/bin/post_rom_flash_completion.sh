f4dbbd232da3dd93438fcd1ce9c5b68554a520ba
AzzyC <azmath2000@gmail.com>
2026-01-01 22:38:54 +0000

    Revert "peridot: Remove DFE related function"
    This reverts commit 55b8c45cc89859764e752cef02ee2905627f36c9.

--- recovery/root/system/bin/post_rom_flash_completion.sh
+++ recovery/root/system/bin/post_rom_flash_completion.sh
@@ -15,15 +15,15 @@ for slot in _a _b; do
 	sleep 1
 done
 
-# LOGMSG "Clearing previous DFE installation logs..."
-# rm -rf /sdcard/neo_file_*
+LOGMSG "Clearing previous DFE installation logs..."
+rm -rf /sdcard/neo_file_*
 
-# LOGMSG "Setting instructions for next reboot..."
-# echo "install /FFiles/DFE.zip" > /cache/recovery/openrecoveryscript
+LOGMSG "Setting instructions for next reboot..."
+echo "install /FFiles/DFE.zip" > /cache/recovery/openrecoveryscript
 
-# LOGMSG "Preserving recovery.log before recovery reboot..."
+LOGMSG "Preserving recovery.log before recovery reboot..."
 LOGMSG "---$SCRIPT_NAME end---"
-# mkdir -p /persist/Fox/logs
-# cp /tmp/recovery.log "/persist/Fox/logs/dfe_recovery_$(date +"%Y%m%d_%H%M%S").log"
+mkdir -p /persist/Fox/logs
+cp /tmp/recovery.log "/persist/Fox/logs/dfe_recovery_$(date +"%Y%m%d_%H%M%S").log"
 
-# reboot recovery
+reboot recovery
--- recovery/root/system/bin/pre_rom_flash.sh
+++ recovery/root/system/bin/pre_rom_flash.sh
@@ -11,8 +11,8 @@ LOGMSG "---$SCRIPT_NAME start---"
 LOGMSG "Resetting SPL date to prevent anti-rollback protection..."
 resetprop ro.build.version.security_patch 2023-12-31
 
-# LOGMSG "Formatting /metadata..."
-# make_f2fs /dev/block/bootdevice/by-name/metadata
+LOGMSG "Formatting /metadata..."
+make_f2fs /dev/block/bootdevice/by-name/metadata
 
 LOGMSG "Detecting active boot slot..."
 slot="$(getprop ro.boot.slot_suffix)"
